e4be32f0e99219ef9039c565ed9128f419a66b7a
vicortavares824 <vitinhotavares824@gmail.com>
2025-10-19 20:13:31 -0300

    atulização quase final

--- app/src/main/java/com/example/apppolarmaquinas/view/SignupScreen.kt
+++ app/src/main/java/com/example/apppolarmaquinas/view/SignupScreen.kt
@@ -1,10 +1,13 @@
 package com.example.apppolarmaquinas.view
 
+import android.R.attr.onClick
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +17,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.apppolarmaquinas.view.component.FancyButton
 
 @Composable
 fun SignupScreen(onBack: () -> Unit = {}) {
@@ -35,9 +39,12 @@ fun SignupScreen(onBack: () -> Unit = {}) {
 
         contentAlignment = Alignment.TopStart
     ) {
-        Button(onClick = onBack) {
-            Text(text = "<")
-        }
+        FancyButton(
+            text = "Voltar",
+            icon = Icons.Default.ArrowBackIosNew,onClick = onBack,
+            gradientColors = listOf(Color(0xFF03A9F4), Color(0x99FF0004))
+
+        )
     }
 }
 
